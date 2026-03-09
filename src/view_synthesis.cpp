e2d8987f9a6fd813082ef4f082a6c64eed415450
Seán Kelly <sean@override.online>
2026-03-09 17:06:52 +0000

    view_synthesis: raise block_size minimum to 3 to prevent crash

--- src/view_synthesis.cpp
+++ src/view_synthesis.cpp
@@ -945,7 +945,7 @@ void SynthWindow::keyPressEvent(QKeyEvent *e)
         changed = true;
     }
     else if (e->key() == Qt::Key_BracketLeft) {
-        block_size = std::max(block_size - 2, 1);
+        block_size = std::max(block_size - 2, 3);
         changed = true;
     }
     else if (e->key() == Qt::Key_1) {
