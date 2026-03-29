c3f5657318dff7741144cfa461b9534eb3a73581
Seán Kelly <sean@override.online>
2026-03-29 22:14:59 +0100

    fix build: add opencv2/calib3d.hpp for cv::filterSpeckles

--- src/view_synthesis.cpp
+++ src/view_synthesis.cpp
@@ -35,6 +35,7 @@
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/cudaarithm.hpp>
 #include <opencv2/cudaimgproc.hpp>
 #include <opencv2/cudawarping.hpp>
